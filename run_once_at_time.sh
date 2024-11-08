db19069449ea2f48f17b20d3c91117b11cad2d7c
Michael Wagdy <76662331+michael-wagdy-l@users.noreply.github.com>
2024-11-08 03:38:12 +0200

    Update run_once_at_time.sh

--- run_once_at_time.sh
+++ run_once_at_time.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Desired time to run the script (24-hour format, e.g., 12:00 for noon)
-target_time="14:19"
+# Desired time to run the script (24-hour format, e.g., 12:00:10 for noon)
+target_time="12:00:10"
 
 while true; do
-    current_time=$(date +%H:%M)
+    current_time=$(date +%H:%M:%S)
     
-    if [ "$current_time" == "$target_time" ]; then
+    # Compare current time with target time
+    if [[ "$current_time" > "$target_time" ]] || [[ "$current_time" == "$target_time" ]]; then
         python ~/storage/downloads/theater.py "مع" 7
         break
     fi
